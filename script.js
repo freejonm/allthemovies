09d3009a2897d8a9ecbc9cc788337826d3479524
Jon Free <freejonm@gmail.com>
2021-01-09 18:30:17 -0500

    linking ajax and testing omdb apiintegration

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("it's working dummy");
+
+$.ajax({
+    url: "https://www.omdbapi.com/?t=jurassic+park&y=&plot=short&apikey=c2a157c7",
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
+
+
