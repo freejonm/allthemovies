b3ede861fb758a87b42b9e83b372e005cbacb3fe
Jon Free <freejonm@gmail.com>
2021-01-11 16:06:14 -0500

    stripped the bootstrap cards to figure out dynamic appending from the movies array

--- script.js
+++ script.js
@@ -10,18 +10,20 @@ var movies = [
 
 function renderCards(){
   for(var i = 0; i < movies.length; i++){
+ 
+    $.ajax({
+      url: "https://www.omdbapi.com/?t=" + movies[i].title +"&y=&plot=short&apikey=c2a157c7",
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
 
-    
-$.ajax({
-    url: "https://www.omdbapi.com/?t=" + movies[i].title +"&y=&plot=short&apikey=c2a157c7",
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-    $("#card-title1").text(response.Title);
-    $("#year-director").text("Released " + response.Released + " Director: " + response.Director);
-    $("#plot1").text("Plot summary: " + response.Plot);
-    $("#cast1").text("Cast: " + response.Actors);
-    $("#awards1").text("Awards info: " + response.Awards);
+      $("body").append(
+        "<div>" + response.Title + "</div>");
+      // $("#card-title1").text(response.Title);
+      // $("#year-director").text("Released " + response.Released + " Director: " + response.Director);
+      // $("#plot1").text("Plot summary: " + response.Plot);
+      // $("#cast1").text("Cast: " + response.Actors);
+      // $("#awards1").text("Awards info: " + response.Awards);
   
   })}
   }
