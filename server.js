137436849726cfb936a74d507419bfa2f6934e58
Jon Free <freejonm@gmail.com>
2021-02-15 12:35:37 -0500

    trying to load static files for styling and script

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const PORT = 3030;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(path.join((__dirname,'Public/style.css'))));
+
+// sending index.html
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+
 
 // listener
 app.listen(PORT, ()=>{console.log(`app listening on ${PORT}`)}); 
